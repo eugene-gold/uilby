839a8c0854ba9f9a2d96526c3c470277d1d9ebd6
Eugene_Gold <uran2@bk.ru>
2023-07-02 13:00:18 +0800

    added theme and theme hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import './index.scss'
+import './styles/index.scss'
 import { Link, Route, Routes } from 'react-router-dom';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { Suspense } from 'react';
+import { useTheme } from './theme/useTheme';
+
+
+
 const App = () => {
+
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Theme</button>
             <Link to={'/about'}>
                 About page
             </Link>
