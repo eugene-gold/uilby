4f60c02376a469651192c9151f3958f534d2e793
Eugene_Gold <uran2@bk.ru>
2023-07-03 10:49:23 +0800

    feature sliced disign tune

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import './styles/index.scss'
 import { Link, Route, Routes } from 'react-router-dom';
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { Suspense } from 'react';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/className/className';
+import { classNames } from 'shared/lib/className/className';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
 
 
 
@@ -24,8 +24,8 @@ const App = () => {
            
             <Suspense fallback={<div>...Loading</div>}>                
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>                
-                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>                
+                    <Route path={'/'} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
         </div>
