01cb39f6632d4f7e902fda5d434e74c0ac144ff1
Eugene_Gold <uran2@bk.ru>
2023-07-03 11:26:01 +0800

    added router config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
 import './styles/index.scss'
-import { Link, Route, Routes } from 'react-router-dom';
-import { Suspense } from 'react';
+import { Link } from 'react-router-dom';
 import { classNames } from 'shared/lib/className/className';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
+import { AppRouter } from './providers/router';
 
 
 
@@ -22,12 +20,7 @@ const App = () => {
                  Main page
             </Link>
            
-            <Suspense fallback={<div>...Loading</div>}>                
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage/>}/>                
-                    <Route path={'/'} element={<MainPage/>}/>
-                </Routes>
-            </Suspense>
+           <AppRouter/>
         </div>
     );
 };
