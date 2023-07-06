0f8d775544fbab3e7f7beb90e3b7c842eb3ca184
Eugene_Gold <uran2@bk.ru>
2023-07-06 15:44:14 +0800

    added babel and extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,6 +4,23 @@ import { BuildOptions } from './types/config'
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
+  const babelLoader = {
+    test: /\.(js|jsx|tsx|ts)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              ["i18next-extract", {
+                locales: ['ru', 'en'],
+                keyAsDefaultValue: true
+              }]
+            ]
+          }
+        }
+  }
+
   const svgLoader = {
     test: /\.svg$/i,
     issuer: /\.[jt]sx?$/,
@@ -50,6 +67,7 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     return [
         fileLoader,
         svgLoader,
+        babelLoader,
         typeScriptLoader,
         cssLoader
       ]
